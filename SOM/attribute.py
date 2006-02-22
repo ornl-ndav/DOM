b47b2aba8e926d6eb16d435283f17076d1e6015f
Pete Peterson <petersonpf@ornl.gov>
2006-02-22 20:44:55 +0000

    Added associated classes for __simple__ SOM

---
+++ SOM/attribute.py
@@ -0,0 +1,3 @@
+class AttributeList(dict):
+    def __init__(self):
+        pass
---
+++ SOM/som.py
@@ -0,0 +1,14 @@
+import so
+import attribute
+
+class SOM(list):
+    TITLE   = "title"
+    X_UNITS = "x_units"
+    Y_UNITS = "y_units"
+    EMPTY   = ""
+
+    def __init__(self):
+        self.attr_list = attribute.AttributeList()
+        self.attr_list[SOM.TITLE]   = SOM.EMPTY
+        self.attr_list[SOM.X_UNITS] = SOM.EMPTY
+        self.attr_list[SOM.Y_UNITS] = SOM.EMPTY
