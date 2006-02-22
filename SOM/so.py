946c5a78baee7d7d6ad6031b2e1299add1aae34e
Pete Peterson <petersonpf@ornl.gov>
2006-02-22 20:29:36 +0000

    Modified class to reflect checkpointing discussion

--- SOM/so.py
+++ SOM/so.py
@@ -1,17 +1,12 @@
 import nessi_vector
 
-class SOM:
+class SO:
 
-    def __init__(self,length=0,):
-        x_length=length
-        if(x_length>0):
-            x_length+=1
-
-        self.x  = nessi_vector.NessiVector(x_length)
-        self.y  = nessi_vector.NessiVector(length)
-        self.dy = nessi_vector.NessiVector(length)
-        self.x_units
-        self.y_units
+    def __init__(self):
+        self.x     = nessi_vector.NessiVector(length)
+        self.y     = nessi_vector.NessiVector(length)
+        self.var_y = nessi_vector.NessiVector(length)
+        self.id    = 0
 
     def __len__(self):
         return len(self.y)
