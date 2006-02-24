8b567eb0deb86b9f62fd73983e7ee39e786e32e7
Michael Reuter <reuterma@ornl.gov>
2006-02-24 22:36:59 +0000

    Checkpointing output formatter test program. This refs #1.

--- test/test_a3c.py
+++ test/test_a3c.py
@@ -14,14 +14,18 @@ SOM1.attr_list["filename"] = filename_SOM1
 SOM1.attr_list["epoch"] = time()
 SOM1.attr_list["timestamp"] = strftime("%Y-%m-%d %T",
                                        localtime(SOM1.attr_list["epoch"]))
+SOM1.attr_list["username"] = "Michael Reuter and the Gang"
+SOM1.attr_list["x_units"] = "microseconds"
+SOM1.attr_list["y_units"] = "beats"
 
 for i in range(2):
     SO1 = SO()
     for j in range(10):
-        SO1.id = j
+        SO1.id = i
         SO1.x.append(j+1)
         SO1.y.append(1000+j+(20*j))
         SO1.var_y.append(100+j)
+    SO1.x.append(11)
 
     SOM1.append(SO1)
 
