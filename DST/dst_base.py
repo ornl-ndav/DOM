6e368a3fdeae8d309a72f8409032ec9a9683a2c6
Michael Reuter <reuterma@ornl.gov>
2006-02-24 14:17:13 +0000

    Adding a few new member functions an correcting a typo.

--- DST/dst_base.py
+++ DST/dst_base.py
@@ -5,7 +5,7 @@ class DST_BASE:
 
     # ----- explicit close
     def release_resource(self):
-        raise NotImpemetedError
+        raise NotImplementedError
 
     # ----- configuration
 
@@ -13,12 +13,21 @@ class DST_BASE:
     def num_avail_SO(self):
         raise NotImplementedError
 
+    def num_avail_SOM(self):
+        raise NotImplementedError
+
     # ----- read information
     def getSO(self,id):
         raise NotImplementedError
 
+    def getSOM(self,id):
+        raise NotImplementedError
+
     # ----- write information
     def writeSO(self,so):
+        raise NotImplementedError
+
+    def writeSOM(self,so):
         raise NotImplementedError
 
 def getInstance(mime_type,resource,*args,**kwargs):
@@ -27,10 +36,13 @@ def getInstance(mime_type,resource,*args,**kwargs):
     my_args.extend(args)
 
     # import the appropriate concrete classes
+    import ascii3col_dst
     import nexus_dst
 
     # do the factory stuff
     if mime_type==nexus_dst.NeXusDST.MIME_TYPE:
         return apply(nexus_dst.NeXusDST,my_args,kwargs)
+    elif mime_type==ascii3col_dst.Ascii3ColDST.MIME_TYPE:
+        return apply(ascii3col_dst.Ascii3ColDST,my_args,kwargs)
     else:
         raise Exception,"something wrong"
