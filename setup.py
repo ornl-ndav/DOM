be82eb4d0df42eb775d49627c4358fb24d757984
Michael Reuter <reuterma@ornl.gov>
2006-02-24 14:42:22 +0000

    Adding python version check. This refs #3.

--- setup.py
+++ setup.py
@@ -7,14 +7,26 @@
 ###############################################################################
 
 from distutils.core import setup
+import sys
 
-package_name = "DOM"
-version_id = "none"
+# Package name and version information
+PACKAGE = "DOM"
+VERSION = "none"
 
+# Minimum version of Python
+PYTHON_MAJOR = 2
+PYTHON_MINOR = 2
+
+if sys.version_info < (PYTHON_MAJOR, PYTHON_MINOR):
+        print >>sys.stderr, 'You need at least Python %d.%d for %s %s' \
+                                      % (PYTHON_MAJOR, PYTHON_MINOR,
+					 PACKAGE, VERSION)
+	sys.exit(3)
+            
 def main():
-    setup(name=package_name,
-          version=version_id,
-          extra_path=package_name,
+    setup(name=PACKAGE,
+          version=VERSION,
+          extra_path=PACKAGE,
           package_dir={"": "."},
           packages=["DST", "SOM"])
 
