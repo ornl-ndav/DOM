eb2dc15e743276dc98a706521fb70bf322138b1a
Pete Peterson <petersonpf@ornl.gov>
2006-02-27 21:09:18 +0000

    Changed to reflect new classes. Refs #2.

--- DST/nexus_dst.py
+++ DST/nexus_dst.py
@@ -1,16 +1,23 @@
 import dst_base
-import sns_nexus
+import nexus_file
 
 class NeXusDST(dst_base.DST_BASE):
     MIME_TYPE="application/x-NeXus"
 
     ########## DST_BASE function
     def __init__(self,resource,*args,**kwargs):
-        self.__nexus=sns_nexus.SNSNeXus(resource)
+        self.__nexus=nexus_file.NeXusFile(resource)
 
     def release_resource(self):
         self.__nexus.close()
 
     ########## special functions
     def list_entries(self):
-        return self.__nexus.ls()
+        listing=[]
+        self.__nexus.initgroupdir()
+        name="blah"
+        while name!=None:
+            (name,type)=self.__nexus.getnextentry()
+            if (name!=None) and (type!="CDF0.0"):
+                listing.append((name,type))
+        return listing
