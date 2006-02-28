118f68c478230463ff4a2fbb56fde7df16ec2e34
Pete Peterson <petersonpf@ornl.gov>
2006-02-28 13:21:54 +0000

    Added instance variable that holds structure information. Refs #2.

--- DST/nexus_dst.py
+++ DST/nexus_dst.py
@@ -7,62 +7,44 @@ class NeXusDST(dst_base.DST_BASE):
     ########## DST_BASE function
     def __init__(self,resource,*args,**kwargs):
         self.__nexus=nexus_file.NeXusFile(resource)
-        listing=self.__build_tree()
-        print listing
+        self.__tree=self.__build_tree()
 
     def release_resource(self):
         self.__nexus.close()
 
     ########## special functions
     def __list_level(self):
-        listing=[]
+        listing={}
         self.__nexus.initgroupdir()
         name="blah"
         while name!=None:
             (name,type)=self.__nexus.getnextentry()
             if (name!=None) and (type!="CDF0.0"):
-                if(type!="SDS"):
-                    type=""
-                listing.append("%s%s" % (name,type))
-        return listing
-
-    def __purge_listing(self,listing):
-        for item in listing:
-            first=listing.index(item)
-            try:
-                while True:
-                    index=listing.index(item,first+1)
-                    del listing[index]
-            except ValueError:
-                pass
+                listing[name]=type
         return listing
 
     def __prepend_parent(self,parent,listing):
-        my_list=[]
-        for item in listing:
-            my_list.append("%s/%s" % (parent,item))
+        my_list={}
+        for key in listing.keys():
+            my_list[("%s/%s" % (parent,key))]=listing[key]
         return my_list
 
-    def __build_tree(self,listing=[]):
+    def __build_tree(self,listing={}):
         # set up result
-        my_listing=[]
-        my_listing.extend(listing)
+        my_listing=listing.copy()
 
         # get a listing for each element in the tree
         if(listing!=None) and (len(listing)>0):
-            for parent in listing:
-                if(not parent.endswith("SDS")):
+            for parent in listing.keys():
+                if(not listing[parent]=="SDS"):
                     self.__nexus.openpath(parent)
                     level_listing=self.__list_level()
-                    if(len(level_listing)>0):
-                        my_listing.extend(self.__prepend_parent(parent,
-                                                                level_listing))
-        # start at the beginning
+                    level_listing=self.__prepend_parent(parent,level_listing)
+                    for inner in level_listing.keys():
+                        my_listing[inner]=level_listing[inner]
+        # or start at the beginning
         else:
             my_listing=self.__prepend_parent("",self.__list_level())
-
-        # remove repetitive items
-        my_listing=self.__purge_listing(my_listing)
 
         # recurse if the list has changed
         if len(my_listing)>len(listing):
@@ -70,9 +52,9 @@ class NeXusDST(dst_base.DST_BASE):
         else:
             return my_listing
         
-
-    def list_entries(self):
-        self.__nexus.openpath("/")
-        listing=self.__list_level()
-
-        return self.__prepend_parent("",listing)
+    def list_type(self,type):
+        my_list=[]
+        for key in self.__tree.keys():
+            if self.__tree[key]==type:
+                my_list.append(key)
+        return my_list
