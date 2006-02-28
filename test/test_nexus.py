46f55ce71cffb0bf9a88a4c4ddd320eca45b5b2a
Pete Peterson <petersonpf@ornl.gov>
2006-02-28 16:48:30 +0000

    Modified test to print ids. Refs #2

--- test/test_nexus.py
+++ test/test_nexus.py
@@ -9,6 +9,10 @@ if __name__=="__main__":
     except IndexError:
         pass # use the default name
     
-    
     dst=dst_base.getInstance("application/x-NeXus",filename)
-    print dst
+    print "**********",filename
+    som_list=dst.get_SOM_ids()
+    for item in som_list:
+        print "FOR SOM:",item
+        print " SO IDS:",dst.get_SO_ids(item)
+        
