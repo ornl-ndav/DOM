a5d75c92f8f009383a881a6b779d8d2af6a6c466
Michael Reuter <reuterma@ornl.gov>
2006-03-01 14:56:29 +0000

    Adding axis label attributes.

--- SOM/som.py
+++ SOM/som.py
@@ -2,13 +2,17 @@ import attribute
 
 class SOM(list):
     TITLE   = "title"
+    X_LABEL = "x_label"
     X_UNITS = "x_units"
+    Y_LABEL = "y_label"
     Y_UNITS = "y_units"
     EMPTY   = ""
 
     def __init__(self):
         self.attr_list = attribute.AttributeList()
         self.attr_list[SOM.TITLE]   = SOM.EMPTY
+        self.attr_list[SOM.X_LABEL] = SOM.EMPTY
         self.attr_list[SOM.X_UNITS] = SOM.EMPTY
+        self.attr_list[SOM.Y_LABEL] = SOM.EMPTY
         self.attr_list[SOM.Y_UNITS] = SOM.EMPTY
         self.dst = None
