8b2bacfe086423bd5ca9f6af0b85d8775a05566b
Michael Reuter <reuterma@ornl.gov>
2006-03-24 14:50:35 +0000

    Changing from NessiVector to NessiList. This resolves #8.

--- SOM/so.py
+++ SOM/so.py
@@ -3,9 +3,9 @@ import nessi_list
 class SO:
 
     def __init__(self):
-        self.x     = nessi_list.NessiList(0)
-        self.y     = nessi_list.NessiList(0)
-        self.var_y = nessi_list.NessiList(0)
+        self.x     = nessi_list.NessiList()
+        self.y     = nessi_list.NessiList()
+        self.var_y = nessi_list.NessiList()
         self.id    = None
 
     def __len__(self):
