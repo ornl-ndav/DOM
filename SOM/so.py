9fa8c9634da8770544d9a28e9ecd5fa866c03083
Pete Peterson <petersonpf@ornl.gov>
2006-03-29 21:36:22 +0000

    Added variance in x. This resolves #10.

--- SOM/so.py
+++ SOM/so.py
@@ -5,6 +5,7 @@ class SO:
     def __init__(self):
         self.x     = nessi_list.NessiList()
         self.y     = nessi_list.NessiList()
+        self.var_x = None
         self.var_y = nessi_list.NessiList()
         self.id    = None
 
@@ -12,8 +13,12 @@ class SO:
         return len(self.y)
 
     def __str__(self):
-        return "("+str(self.id)+", "+str(self.x)+", "+str(self.y)+\
-               ", "+str(self.var_y)+")"
+        if self.var_x==None:
+            return "("+str(self.id)+", "+str(self.x)+", "+str(self.y)+\
+                   ", "+str(self.var_y)+")"
+        else:
+            return "("+str(self.id)+", "+str(self.x)+", "+str(self.y)+\
+                   ", "+str(self.var_x)+", "+str(self.var_y)+")"
 
     def __repr__(self):
         return self.__str__()
