35299ceeb70c0ae397366102606580d876036e9a
Pete Peterson <petersonpf@ornl.gov>
2006-04-04 12:24:08 +0000

    Exposed all functions, now need to implement them.

--- nexus/sns_napi.cpp
+++ nexus/sns_napi.cpp
@@ -14,6 +14,7 @@ static void NeXusFile_privateclose(void *file)
   return;
 }
 
+//NXopen(filename,access)
 static PyObject * NeXusFile_open(PyObject *, PyObject *args)
 {
   // set default access method
@@ -23,8 +24,6 @@ static PyObject * NeXusFile_open(PyObject *, PyObject *args)
   // get the arguments out
   if(!PyArg_ParseTuple(args,"s|i",&filename,&access))
     return NULL;
-
-  std::cout << "open(" << filename << "," << access << ")" << std::endl;
 
   // open the file
   NXhandle handle;
@@ -36,7 +35,13 @@ static PyObject * NeXusFile_open(PyObject *, PyObject *args)
   // convert the handle to python
   return PyCObject_FromVoidPtr(handle,NeXusFile_privateclose);
 }
+
 //NXmakegroup(handle,name,class) // NEEDS IMPLEMENTATION
+static PyObject *NeXusFile_makegroup(PyObject *, PyObject *args)
+{
+  PyErr_SetString(PyExc_NotImplementedError,"Writing is not implemented");
+  return NULL;
+}
 
 //NXopengroup(handle,name,class) // NEEDS IMPLEMENTATION
 static PyObject *NeXusFile_opengroup(PyObject *, PyObject *args)
@@ -79,64 +84,261 @@ static PyObject *NeXusFile_closegroup(PyObject *, PyObject *args)
 }
 
 //NXopenpath(handle,path) // NEEDS IMPLEMENTATION
+static PyObject *NeXusFile_openpath(PyObject *, PyObject *args)
+{
+  // get the arguments
+  char *path;
+  PyObject *pyhandle;
+  if(!PyArg_ParseTuple(args,"Os",&pyhandle,&path))
+    return NULL;
+  NXhandle handle=static_cast<NXhandle>(PyCObject_AsVoidPtr(pyhandle));
+
+  // do the work
+  if(NXopenpath(handle,path)!=NX_OK){
+    PyErr_SetString(PyExc_IOError,"openpath failed");
+    return NULL;
+  }
+
+  Py_INCREF(Py_None);
+  return Py_None;
+}
 
 //NXopengrouppath(handle,path) // NEEDS IMPLEMENTATION
+static PyObject *NeXusFile_opengrouppath(PyObject *, PyObject *args)
+{
+  Py_INCREF(Py_None);
+  return Py_None;
+}
 
 //NXmakedata(handle,name,type,rank,dimensions[]) // NEEDS IMPLEMENTATION
+static PyObject *NeXusFile_makedata(PyObject *, PyObject *args)
+{
+  PyErr_SetString(PyExc_NotImplementedError,"Writing is not implemented");
+  return NULL;
+}
 
 //NXcompmakedata(handle,name,type,rank,dimensions[],compression) // NEEDS IMPLEMENTATION
+static PyObject *NeXusFile_compmakedata(PyObject *, PyObject *args)
+{
+  PyErr_SetString(PyExc_NotImplementedError,"Writing is not implemented");
+  return NULL;
+}
 
 //NXopendata(handle,name) // NEEDS IMPLEMENTATION
+static PyObject *NeXusFile_opendata(PyObject *, PyObject *args)
+{
+  Py_INCREF(Py_None);
+  return Py_None;
+}
 
 //NXcompress(handle,compression) // NEEDS IMPLEMENTATION
+static PyObject *NeXusFile_compress(PyObject *, PyObject *args)
+{
+  PyErr_SetString(PyExc_NotImplementedError,"Writing is not implemented");
+  return NULL;
+}
 
 //NXclosedata(handle) // NEEDS IMPLEMENTATION
+static PyObject *NeXusFile_closedata(PyObject *, PyObject *args)
+{
+  Py_INCREF(Py_None);
+  return Py_None;
+}
 
 //NXgetdata(handle,data) // NEEDS IMPLEMENTATION
+static PyObject *NeXusFile_getdata(PyObject *, PyObject *args)
+{
+  Py_INCREF(Py_None);
+  return Py_None;
+}
 
 //NXgetslab(handle,data,start[],size[]) // NEEDS IMPLEMENTATION
+static PyObject *NeXusFile_getslab(PyObject *, PyObject *args)
+{
+  Py_INCREF(Py_None);
+  return Py_None;
+}
 
 //NXgetattr(handle,name,value,length,type) // NEEDS IMPLEMENTATION
+static PyObject *NeXusFile_getattr(PyObject *, PyObject *args)
+{
+  Py_INCREF(Py_None);
+  return Py_None;
+}
 
 //NXputdata(handle,data) // NEEDS IMPLEMENTATION
+static PyObject *NeXusFile_putdata(PyObject *, PyObject *args)
+{
+  PyErr_SetString(PyExc_NotImplementedError,"Writing is not implemented");
+  return NULL;
+}
 
 //NXputslab(handle,data,start[],size[]) // NEEDS IMPLEMENTATION
+static PyObject *NeXusFile_putslab(PyObject *, PyObject *args)
+{
+  PyErr_SetString(PyExc_NotImplementedError,"Writing is not implemented");
+  return NULL;
+}
 
 //NXputattr(handle,name,value,length,type) // NEEDS IMPLEMENTATION
+static PyObject *NeXusFile_putattr(PyObject *, PyObject *args)
+{
+  PyErr_SetString(PyExc_NotImplementedError,"Writing is not implemented");
+  return NULL;
+}
 
 //NXflush(handle) // NEEDS IMPLEMENTATION
+static PyObject *NeXusFile_flush(PyObject *, PyObject *args)
+{
+  Py_INCREF(Py_None);
+  return Py_None;
+}
 
 //NXgetinfo(handle,rank,dimension[],type) // NEEDS IMPLEMENTATION
+static PyObject *NeXusFile_getinfo(PyObject *, PyObject *args)
+{
+  Py_INCREF(Py_None);
+  return Py_None;
+}
 
 //NXgetgroupinfo(handle,item_number,name,class) // NEEDS IMPLEMENTATION
+static PyObject *NeXusFile_getgroupinfo(PyObject *, PyObject *args)
+{
+  Py_INCREF(Py_None);
+  return Py_None;
+}
 
 //NXinitgroupdir(handle) // NEEDS IMPLEMENTATION
+static PyObject *NeXusFile_initgroupdir(PyObject *, PyObject *args)
+{
+  Py_INCREF(Py_None);
+  return Py_None;
+}
 
-//NXgetnextenty(handle,name,class,type) // NEEDS IMPLEMENTATION
+//NXgetnextentry(handle,name,class,type) // NEEDS IMPLEMENTATION
+static PyObject *NeXusFile_getnextentry(PyObject *, PyObject *args)
+{
+  Py_INCREF(Py_None);
+  return Py_None;
+}
 
 //NXgetattrinfo(handle,num_attrs) // NEEDS IMPLEMENTATION
+static PyObject *NeXusFile_getattrinfo(PyObject *, PyObject *args)
+{
+  Py_INCREF(Py_None);
+  return Py_None;
+}
 
 //NXinitattrdir(handle) // NEEDS IMPLEMENTATION
+static PyObject *NeXusFile_initattrdir(PyObject *, PyObject *args)
+{
+  Py_INCREF(Py_None);
+  return Py_None;
+}
 
 //NXgetnextattr(handle,name,length,type) // NEEDS IMPLEMENTATION
+static PyObject *NeXusFile_getnextattr(PyObject *, PyObject *args)
+{
+  Py_INCREF(Py_None);
+  return Py_None;
+}
 
 //NXgetgroupID(handle,link_id) // NEEDS IMPLEMENTATION
+static PyObject *NeXusFile_getgroupID(PyObject *, PyObject *args)
+{
+  Py_INCREF(Py_None);
+  return Py_None;
+}
 
 //NXgetdataID(handle,link_id) // NEEDS IMPLEMENTATION
+static PyObject *NeXusFile_getdataID(PyObject *, PyObject *args)
+{
+  Py_INCREF(Py_None);
+  return Py_None;
+}
 
 //NXmakelink(handle,link_id) // NEEDS IMPLEMENTATION
+static PyObject *NeXusFile_makelink(PyObject *, PyObject *args)
+{
+  PyErr_SetString(PyExc_NotImplementedError,"Writing is not implemented");
+  return NULL;
+}
 
 //NXmalloc(data,rank,dimesinos[],type) // NEEDS IMPLEMENTATION
+static PyObject *NeXusFile_malloc(PyObject *, PyObject *args)
+{
+  PyErr_SetString(PyExc_NotImplementedError,"Should be unnecessary");
+  return NULL;
+}
 
 //NXfree(data) // NEEDS IMPLEMENTATION
+static PyObject *NeXusFile_free(PyObject *, PyObject *args)
+{
+  PyErr_SetString(PyExc_NotImplementedError,"Should be unnecessary");
+  return NULL;
+}
 
 static PyMethodDef NeXusFile_methods[]={
-  {"open",      (PyCFunction)NeXusFile_open, METH_VARARGS,
-   "Open the file"},
-  {"opengroup", (PyCFunction)NeXusFile_opengroup, METH_VARARGS,
-   "Open the group specified by the name and class"},
-  {"closegroup",(PyCFunction)NeXusFile_closegroup,METH_VARARGS,
-   "Close the currently open group"},
+  {"open",         (PyCFunction)NeXusFile_open, METH_VARARGS,
+   "Default access is read"},
+  {"makegroup",    (PyCFunction)NeXusFile_makegroup, METH_VARARGS,
+   ""},
+  {"opengroup",    (PyCFunction)NeXusFile_opengroup, METH_VARARGS,
+   ""},
+  {"closegroup",   (PyCFunction)NeXusFile_closegroup,METH_VARARGS,
+   ""},
+  {"openpath",     (PyCFunction)NeXusFile_openpath, METH_VARARGS,
+   ""},
+  {"opengrouppath",(PyCFunction)NeXusFile_opengrouppath, METH_VARARGS,
+   ""},
+  {"makedata",     (PyCFunction)NeXusFile_makedata, METH_VARARGS,
+   ""},
+  {"compmakedata", (PyCFunction)NeXusFile_compmakedata, METH_VARARGS,
+   ""},
+  {"opendata",     (PyCFunction)NeXusFile_opendata, METH_VARARGS,
+   ""},
+  {"compress",     (PyCFunction)NeXusFile_compress, METH_VARARGS,
+   ""},
+  {"closedata",    (PyCFunction)NeXusFile_closedata, METH_VARARGS,
+   ""},
+  {"getdata",      (PyCFunction)NeXusFile_getdata, METH_VARARGS,
+   ""},
+  {"getslab",      (PyCFunction)NeXusFile_getslab, METH_VARARGS,
+   ""},
+  {"getattr",      (PyCFunction)NeXusFile_getattr, METH_VARARGS,
+   ""},
+  {"putdata",      (PyCFunction)NeXusFile_putdata, METH_VARARGS,
+   ""},
+  {"putslab",      (PyCFunction)NeXusFile_putslab, METH_VARARGS,
+   ""},
+  {"putattr",      (PyCFunction)NeXusFile_putattr, METH_VARARGS,
+   ""},
+  {"flush",        (PyCFunction)NeXusFile_flush, METH_VARARGS,
+   ""},
+  {"getinfo",      (PyCFunction)NeXusFile_getinfo, METH_VARARGS,
+   ""},
+  {"getgroupinfo", (PyCFunction)NeXusFile_getgroupinfo, METH_VARARGS,
+   ""},
+  {"initgroupdir", (PyCFunction)NeXusFile_initgroupdir, METH_VARARGS,
+   ""},
+  {"getnextentry", (PyCFunction)NeXusFile_getnextentry, METH_VARARGS,
+   ""},
+  {"getattrinfo",  (PyCFunction)NeXusFile_getattrinfo, METH_VARARGS,
+   ""},
+  {"initattrdir",  (PyCFunction)NeXusFile_initattrdir, METH_VARARGS,
+   ""},
+  {"getnextattr",  (PyCFunction)NeXusFile_getnextattr, METH_VARARGS,
+   ""},
+  {"getgroupID",   (PyCFunction)NeXusFile_getgroupID, METH_VARARGS,
+   ""},
+  {"getdataID",    (PyCFunction)NeXusFile_getdataID, METH_VARARGS,
+   ""},
+  {"makelink",     (PyCFunction)NeXusFile_makelink, METH_VARARGS,
+   ""},
+  {"malloc",       (PyCFunction)NeXusFile_malloc, METH_VARARGS,
+   ""},
+  {"free",         (PyCFunction)NeXusFile_free, METH_VARARGS,
+   ""},
   {NULL,NULL}
 };
 
