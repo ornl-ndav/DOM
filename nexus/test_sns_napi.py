c1e36e2fec5a25466233664faba42bf6ae0a49a2
Pete Peterson <petersonpf@ornl.gov>
2006-04-05 18:02:27 +0000

    Demonstrates some more features of sns_napi and adds benchmarks for reading out data. Refs #12.

--- nexus/test_sns_napi.py
+++ nexus/test_sns_napi.py
@@ -1,4 +1,12 @@
 import sns_napi
+import nessi_list
+import timing
+
+def print_mark(tag,milli):
+    sec=(milli-(milli%1000))/1000
+    milli=milli-sec*1000
+    print "%18s :%02d.%03d" % (tag,sec,milli)
+
 print dir(sns_napi)
 #help(sns_napi)
 handle=sns_napi.open("/home/pf9/DAS_2.nxs")
@@ -9,11 +17,34 @@ print "getnextentry()",sns_napi.getnextentry(handle)
 print "opendata(x_pixel_offset)",sns_napi.opendata(handle,"x_pixel_offset")
 print "getattr(axis)",sns_napi.getattr(handle,"axis")
 print "getattr(units)",sns_napi.getattr(handle,"units")
-print "getdata()",len(sns_napi.getdata(handle))
+data=sns_napi.getdata(handle)
+nessi_data=nessi_list.NessiList(len(data))
+sns_napi.copysequence(data,nessi_data)
+print "getdata()",len(sns_napi.getdata(handle)),nessi_data
 print "closedata()",sns_napi.closedata(handle)
 print "opendata(data)",sns_napi.opendata(handle,"data")
+(dims,type)=sns_napi.getinfo(handle)
+print "getinfo()",(dims,type)
 print "getnextattr()",sns_napi.getnextattr(handle)
-print "***** getslab()",len(sns_napi.getslab(handle,(0,0,0),(0,0,167)))
-print "***** getslab()",len(sns_napi.getslab(handle,(0,0,0),(0,0,167)))
-print "***** getslab()",len(sns_napi.getslab(handle,(1,0,0),(1,0,167)))
+length=1
+for item in dims:
+    length*=item
+print "getdata benchmark [:sec.milli]"
+timing.start()
+data=sns_napi.getdata(handle)
+timing.finish()
+print_mark("List",timing.milli())
+data=nessi_list.NessiList(length)
+data=nessi_list.NessiList()
+timing.start()
+data=sns_napi.getdata(handle,data)
+timing.finish()
+print_mark("NessiList(unsized)",timing.milli())
+data=nessi_list.NessiList(length)
+timing.start()
+data=sns_napi.getdata(handle,data)
+timing.finish()
+print_mark("NessiList(sized)",timing.milli())
+print "getslab((0,0,0),(0,0,167))",len(sns_napi.getslab(handle,(0,0,0),(0,0,167)))
+print "getslab((1,0,0),(1,0,167))",len(sns_napi.getslab(handle,(1,0,0),(1,0,167)))
 
