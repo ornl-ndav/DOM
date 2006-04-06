67a28d0e4df5ce5e72941a746f8240d5f8d1604b
Michael Reuter <reuterma@ornl.gov>
2006-04-06 14:36:10 +0000

    New additions for building sns_napi. This resolves #13.

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@
 #
 ###############################################################################
 
-
-from distutils.core import setup
+from distutils.core import setup, Extension
+import os
 import sys
 
 # Package name and version information
@@ -20,17 +20,30 @@ PYTHON_MAJOR = 2
 PYTHON_MINOR = 2
 
 if sys.version_info < (PYTHON_MAJOR, PYTHON_MINOR):
-        print >>sys.stderr, 'You need at least Python %d.%d for %s %s' \
+        print >> sys.stderr, 'You need at least Python %d.%d for %s %s' \
                                       % (PYTHON_MAJOR, PYTHON_MINOR,
 					 PACKAGE, VERSION)
 	sys.exit(3)
+
+nexus_libdir = '/usr/lib/'
+libdir_list = [nexus_libdir]
+
+nexus_lib = "NeXus"
+lib_list_all = [nexus_lib]
+
+if os.uname()[0] == 'Linux':
+            lib_list_all.append('stdc++')
             
 def main():
     setup(name=PACKAGE,
           version=VERSION,
           extra_path=PACKAGE,
           package_dir={"": "."},
-          packages=["DST", "SOM"])
+          packages=["DST", "SOM"],
+          ext_modules = [Extension( "sns_napi", ["nexus/sns_napi.cpp"] ,
+                                    library_dirs = libdir_list,
+                                    libraries = lib_list_all)],
+          )
 
 if __name__ == "__main__":
     main()
