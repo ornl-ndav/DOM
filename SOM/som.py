b2fb51aad739a6d7e05ea5392b6c2ddad23c2b87
Michael Reuter <reuterma@ornl.gov>
2006-04-10 18:34:20 +0000

    Added an important core attribute. This refs #15.

--- SOM/som.py
+++ SOM/som.py
@@ -9,6 +9,7 @@ class SOM(list):
         self.__axis_units__ = []
         self.__y_label__ = SOM.EMPTY
         self.__y_units__ = SOM.EMPTY
+        self.__data_set_type__ = SOM.EMPTY
         
         self.attr_list = attribute.AttributeList()
         self.dst = None
@@ -24,6 +25,7 @@ class SOM(list):
 
     def copyAttributes(self,other):
         self.setTitle(other.getTitle())
+        self.setDataSetType(other.getDataSetType())
         self.setAllAxisLabels(other.getAllAxisLabels())
         self.setAllAxisUnits(other.getAllAxisUnits())
         self.setYLabel(other.getYLabel())
@@ -43,6 +45,9 @@ class SOM(list):
     
     def getAxisUnits(self,dim=0):
         return self.__axis_units__[dim]
+
+    def getDataSetType(self):
+        return self.__data_set_type__
 
     def getTitle(self):
         return self.__title__
@@ -65,6 +70,9 @@ class SOM(list):
     def setAxisUnits(self,dim,units):
         self.__axis_units__[dim] = units
 
+    def setDataSetType(self,type):
+        self.__data_set_type__ = type
+
     def setTitle(self,title):
         self.__title__ = title
 
@@ -80,6 +88,7 @@ if __name__ == "__main__":
     import so
     som = SOM()
     som.setTitle("Test SOM")
+    som.setDataSetType("histogram")
     
     so1 = so.SO(withVar=True)
     so1.axis[0].val.extend([1.0,2.0,3.0])
