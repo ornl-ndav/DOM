f9186ce9d9aae49c9676395347ccadde5acddb65
Michael Reuter <reuterma@ornl.gov>
2006-04-11 14:32:05 +0000

    Adding forgotten file for distutils. This refs #14.

---
+++ __init__.py
@@ -0,0 +1,10 @@
+from DST import ascii3col_dst
+from DST import dst_base
+from DST import nexus_dst
+from SOM import attribute
+from SOM import som
+from SOM import so
+
+#version
+__id__ = "$Id$"
+
