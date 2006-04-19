3acda808b5d5bf9a4b9b4ba5cc2413906c1c5beb
Pete Peterson <petersonpf@ornl.gov>
2006-04-19 21:23:23 +0000

    Added code to standardize units. Refs #7.

--- SOM/igs_instrument.py
+++ SOM/igs_instrument.py
@@ -5,17 +5,26 @@ class IGS_Instrument(Instrument):
         self.set_primary(84)
         self.__L2=None
     
-    def get_L2(self,id=None):
-        return self.__L2
+    def get_L2(self,id=None,**kwargs):
+        units=__get_units(kwargs,"meter")
+        if units=="meter":
+            return self.__L2
+        else:
+            raise RuntimeError,"Do not understand units \"%s\"" % units
     
     def set_L2(self,distance,units="meter",id=None):
+        units=__standardize_units(units,"meter")
         self.__L2=distance
 
-    def get_L3(self,id=None):
-        return 0.
+    def get_L3(self,id=None,**kwargs):
+        units=__get_units(kwargs,"meter")
+        if units=="meter":
+            return 0.
+        else:
+            raise RuntimeError,"Do not understand units \"%s\"" % units
 
     def set_L3(self,distances,units="meter",id=None):
-        pass
+        units=__standardize_units(units,"meter")
 
     def get_secondary(self,id=None,**kwargs):
         """The secondary flight path (neutronic distance from sample
@@ -25,7 +34,7 @@ class IGS_Instrument(Instrument):
          units="meter" to specify what units the returned value will be in."""
 
         # parse the keywords
-        units=__get_units(kwargs,"degree")
+        units=__get_units(kwargs,"meter")
 
         # calculate the position
         result=self.get_L2(id)+self.get_L3(id)
@@ -51,7 +60,7 @@ class IGS_Instrument(Instrument):
         raise RuntimeError,"Polar angle is not defined"
 
     def set_polar(self,angles,units="radian",id=None):
-        pass
+        units=__standardize_units(units,"radian")
 
     def get_azimuthal(self,id=None,**kwargs):
         """The azimuthal angle (angle between plane and detector) in
@@ -66,13 +75,27 @@ class IGS_Instrument(Instrument):
         raise RuntimeError,"Azimuthal angle is not defined"
 
     def set_azimuthal(self,angles,units="radian",id=None):
-        pass
+        units=__standardize_units(units,"radian")
 
     def __standardize_units(units,default_units):
         # return early if the requested and specified are the same
         if units==default_units:
             return default_units
 
+        # check if it is just plural form of the default units
+        if units==default_units+"s":
+            return default_units
+
+        # the real work of standardization
+        if units=="m" or units=="meter" or units=="meters" \
+           or units=="metre" or units=="metres":
+            return "meter"
+        if units=="degree" or units=="degrees":
+            return "degree"
+        if units=="radian" or units=="radians":
+            return "radian"
+
+        # give up
         raise RuntimeError,"Do not understand units \"%s\"" % units
 
     def __get_units(kwargs,default_units):
@@ -81,5 +104,6 @@ class IGS_Instrument(Instrument):
             return default_val
         units=kwargs["units"]
 
+        # let somebody else do the work
         return __standardize_units(units,default_units)
 
