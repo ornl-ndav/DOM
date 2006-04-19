8497149097abf50d47b2fc77adcc06db692bb133
Pete Peterson <petersonpf@ornl.gov>
2006-04-19 19:05:29 +0000

    Added initial version of instrument and sample. Refs #7 and #16.

--- SOM/instrument.py
+++ SOM/instrument.py
@@ -1,4 +1,23 @@
 class Instrument:
+    """This is an abstract base class representing important
+    geometrical information about the instrument."""
     def __init__(self):
         pass
     
+    def get_primary(id=None):
+        """The primary flight path (neutronic distance from moderator
+        to sample)"""
+        return 0
+
+    def get_secondary(id=None):
+        """The secondary flight path (neutronic distance from sample
+        to detector)"""
+        return 0
+
+    def get_polar(id=None):
+        """The polar angle (angle between incident beam and detector)"""
+        return 0
+
+    def get_azimuthal(id=None):
+        """The azimuthal angle (angle between plane and detector)"""
+        return 0
--- SOM/sample.py
+++ SOM/sample.py
@@ -1,3 +1,5 @@
 class Sample:
+    """This is an abstract base class representing important
+    information about the sample."""
     def __init__(self):
-        pass
+        self.name=""
