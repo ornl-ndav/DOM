6762ab5145c2faa8a2aca89d055d6f87982f18d9
Pete Peterson <petersonpf@ornl.gov>
2006-05-23 14:57:25 +0000

    Modified to set the 'instrument' and 'sample' to 'None' by default. They can be supplied using command line arguments. This resolves #16.

--- SOM/attribute.py
+++ SOM/attribute.py
@@ -1,6 +1,19 @@
 class AttributeList(dict):
-    def __init__(self):
-        import instrument
-        import sample
-        self.instrument=instrument.Instrument()
-        self.sample=sample.Sample()
+    def __init__(self,**kwargs):
+        """
+        Parameters:
+        -----------
+        ->kwargs is a list of key word arguments that the function accepts:
+             instrument = the instrument for the AttributeList to hold
+             sample     = the sample for the AttributeList to hold
+        """
+
+        try:
+            self.instrument=kwargs["instrument"]
+        except KeyError:
+            self.instrument=None
+
+        try:
+            self.sample=kwargs["sample"]
+        except KeyError:
+            self.sample=None
