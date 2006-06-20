61e585425db231fafe91b001a74e2f08b4099ee2
Michael Reuter <reuterma@ornl.gov>
2006-06-20 14:34:28 +0000

    Adding requested features. This resolves #52.

--- DST/ascii3col_dst.py
+++ DST/ascii3col_dst.py
@@ -37,6 +37,7 @@ class Ascii3ColDST(dst_base.DST_BASE):
 
 
     def writeSOM(self,som):
+        self.__data_type = som.getDataSetType()
         self.writeHeader(som)
         self.__counter = 1
         for so in som:
@@ -70,27 +71,53 @@ class Ascii3ColDST(dst_base.DST_BASE):
         names.append(som.getYUnits())
         names.append(som.getYUnits())
 
-        return result, tuple(names)
+        return (result, tuple(names))
 
     def writeHeader(self,som):
-        print >> self.__file, "#F",som.attr_list["filename"]
+        try:
+            som.attr_list["filename"].reverse()
+            som.attr_list["filename"].reverse()
+            for file in som.attr_list["filename"]:
+                print >> self.__file, "#F", file
+        except AttributeError:
+            print >> self.__file, "#F",som.attr_list["filename"]
+
         print >> self.__file, "#E",self.__epoch
         print >> self.__file, "#D",self.__timestamp
+        
         if som.attr_list.has_key("run_number"):
             print >> self.__file, "#C Run Number:",som.attr_list["run_number"]
+        else:
+            pass
+        
         print >> self.__file, "#C Title:",som.getTitle()
+
         if som.attr_list.has_key("username"):
             print >> self.__file, "#C User:",som.attr_list["username"]
+        else:
+            pass
+
+        if som.attr_list.has_key("detector_angle"):
+            print >> self.__file, "#C Detector Angle:",\
+                  som.attr_list["detector_angle"]
+        else:
+            pass
+        
         if som.attr_list.has_key("operations"):
             for op in som.attr_list["operations"]:
                 print >> self.__file, "#C Operation",op
+        else:
+            pass
+        
         if som.attr_list.has_key("parents"):
             print >> self.__file, "#C Parent Files"
             pdict = som.attr_list["parents"]
             for key in pdict:
                 print >> self.__file, "#C %s: %s" % (key, pdict[key]) 
-
-        format_str, names = self.__formatDataInfo(som)
+        else:
+            pass
+        
+        (format_str, names) = self.__formatDataInfo(som)
         self.__axes_and_units =  format_str % names
 
 
@@ -100,7 +127,15 @@ class Ascii3ColDST(dst_base.DST_BASE):
         print >> self.__file, "#N", self.COLUMNS
         print >> self.__file, self.__axes_and_units
         so_y_len=len(so.y)
-        for i in range(len(so)+1):
+
+        size = so_y_len
+        if self.__data_type == "" or self.__data_type == "histogram":
+            size += 1
+        # Density data does not need to be incremented by one
+        else:
+            pass
+        
+        for i in range(size):
             dim = so.dim()
             for j in range(dim):
                 print >> self.__file, so.axis[j].val[i],self.SPACE,
