0dee098eae3add67e25ea60f44ba0f2bc4054290
Michael Reuter <reuterma@ornl.gov>
2006-08-09 15:52:50 +0000

    Added writing of notes to the ascii file. This resolves #64.

--- DST/ascii3col_dst.py
+++ DST/ascii3col_dst.py
@@ -91,6 +91,7 @@ class Ascii3ColDST(dst_base.DST_BASE):
             pass
         
         print >> self.__file, "#C Title:",som.getTitle()
+        print >> self.__file, "#C Notes:", som.attr_list["notes"]
 
         if som.attr_list.has_key("username"):
             print >> self.__file, "#C User:",som.attr_list["username"]
