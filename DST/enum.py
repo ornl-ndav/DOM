358898cb06a1dbcbf289d0df337c5f3f563cffdc
Michael Reuter <reuterma@ornl.gov>
2007-03-05 02:19:36 +0000

    Adding copyright statements to files. This resolves #28.

--- DST/enum.py
+++ DST/enum.py
@@ -1,3 +1,25 @@
+#                        Data Object Model
+#           A part of the SNS Analysis Software Suite.
+#
+#                  Spallation Neutron Source
+#          Oak Ridge National Laboratory, Oak Ridge TN.
+#
+#
+#                             NOTICE
+#
+# For this software and its associated documentation, permission is granted
+# to reproduce, prepare derivative works, and distribute copies to the public
+# for any purpose and without fee.
+#
+# This material was prepared as an account of work sponsored by an agency of
+# the United States Government.  Neither the United States Government nor the
+# United States Department of Energy, nor any of their employees, makes any
+# warranty, express or implied, or assumes any legal liability or
+# responsibility for the accuracy, completeness, or usefulness of any
+# information, apparatus, product, or process disclosed, or represents that
+# its use would not infringe privately owned rights.
+#
+
 class Enum:
 
     """Taken from http://www.novig.com/python-iaq.html
