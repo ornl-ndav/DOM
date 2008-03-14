c1dade05c80765e52037d517ed84ecf5737345ab
Michael Reuter <reuterma@ornl.gov>
2008-03-14 18:28:44 +0000

    Adding a test script. This refs #213.

---
+++ test/test_read_numinfo.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+#                        Data Object Model
+#           A part of the SNS Analysis Software Suite.
+#
+#                  Spallation Neutron Source
+#          Oak Ridge National Laboratory, Oak Ridge TN.
+#
+#
+#                             NOTICE
+#
+# For this software and its associated documentation, permission is granted
+# to reproduce, prepare derivative works, and distribute copies to the public
+# for any purpose and without fee.
+#
+# This material was prepared as an account of work sponsored by an agency of
+# the United States Government.  Neither the United States Government nor the
+# United States Department of Energy, nor any of their employees, makes any
+# warranty, express or implied, or assumes any legal liability or
+# responsibility for the accuracy, completeness, or usefulness of any
+# information, apparatus, product, or process disclosed, or represents that
+# its use would not infringe privately owned rights.
+#
+
+# $Id$
+
+import DST
+
+filename = "/SNS/users/2zr/data/test/BSS/20071005/BSS_126.norm"
+
+ifile = open(filename, "r")
+
+nif = DST.NumInfoDST(ifile)
+
+som = nif.getSOM()
+
+nif.release_resource()
+
+print som.attr_list
+print som
