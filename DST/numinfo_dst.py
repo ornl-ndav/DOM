b878c2bd1eb18a5e526f2cae2c0185519d9720f7
Michael Reuter <reuterma@ornl.gov>
2008-03-17 19:05:44 +0000

    Adding data reading capability. Can now read in an entire file. This resolves #213.

--- DST/numinfo_dst.py
+++ DST/numinfo_dst.py
@@ -191,7 +191,32 @@ class NumInfoDST(dst_base.DST_BASE):
     ########## Special functions
 
     def readSO(self, som, dline):
-        pass
+        """
+        This method reads the data lines and creates the appropriate SOs for
+        the data.
+
+        @param som: The object to have its information set from file.
+        @type som: L{SOM.SOM}
+
+        @param dline: The line from the file containing the data
+        @type dline: C{string}        
+        """
+        import nessi_list
+        
+        parts = dline.split()
+
+        so = SOM.SO()
+        so.y = nessi_list.NessiList()
+        so.var_y = nessi_list.NessiList()
+
+        # The spectrum ID is the first three slots
+        so.id = SOM.NeXusId.fromList(parts[0:3]).toTuple()
+        # Get the value
+        so.y.append(float(parts[-2]))
+        # Need to square the error since we carry around error^2
+        so.var_y.append(float(parts[-1]) * float(parts[-1]))
+
+        som.append(so)
 
     def readSOM(self, som, lline):
         """
