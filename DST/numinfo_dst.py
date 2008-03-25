f20a9d4427a0115c63e226e4cb6a5e70b8117a09
Michael Reuter <reuterma@ornl.gov>
2008-03-25 20:30:22 +0000

    Switching to new {{{Roi}}} class. This resolves #237.

--- DST/numinfo_dst.py
+++ DST/numinfo_dst.py
@@ -138,21 +138,12 @@ class NumInfoDST(dst_base.DST_BASE):
         try:
             roi_filename = kwargs["roi_file"]
             try:
-                roi_file = open(roi_filename, "r")
-            except IOError:
-                raise RuntimeError("Cannot open roi file %s" % roi_filename)
-            
-            roi_id_list = []
-            
-            for pixel_id_line in roi_file:
-                if pixel_id_line.startswith("#"):
-                    continue
-                
-                roi_id_list.append(SOM.NeXusId.fromString(\
-                    pixel_id_line.rstrip()))
-                                   
+                roi = SOM.Roi(roi_filename)
+            except TypeError:
+                # roi_filename is None
+                roi = None
         except KeyError:
-            roi_file = None
+            roi = None
         
         som = SOM.SOM()
 
@@ -178,8 +169,8 @@ class NumInfoDST(dst_base.DST_BASE):
                         filter_so = True
 
                 # Check the ROI list
-                if roi_file is not None:
-                    if so_id not in roi_id_list:
+                if roi is not None:
+                    if so_id not in roi:
                         filter_so = True
 
                 if not filter_so:
