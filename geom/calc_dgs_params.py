fb778254eaa95f38911e5b8afcd9eb653cdfd9bd
Michael Reuter <reuterma@ornl.gov>
2008-10-03 18:32:46 +0000

    Getting ordered list of bank numbers. This refs #267.

--- geom/calc_dgs_params.py
+++ geom/calc_dgs_params.py
@@ -9,3 +9,12 @@ timer = sns_timing.DiffTime()
 data_dst = DST.getInstance("application/x-NeXus", filename) 
 timer.getTime(msg="After reading data ")
 
+SOM_ids = data_dst.get_SOM_ids()
+
+# Get the bank numbers sorted in proper order
+bank_list = [SOM_id[0].split('/')[-1] for SOM_id in SOM_ids]
+bank_nums = [int(id.replace('bank', '')) for id in bank_list
+             if not id.startswith("monitor")]
+bank_nums.sort()
+
+
