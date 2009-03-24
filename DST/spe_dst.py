2c7d53016aef77584b35bbc6021cb5a46d514ca7
Stuart Campbell <stuart@stuartcampbell.me>
2009-03-24 15:55:50 +0000

    Removed the newline after each value. refs #291

--- DST/spe_dst.py
+++ DST/spe_dst.py
@@ -133,7 +133,7 @@ class SpeDST(dst_base.DST_BASE):
 
         print >> self.__file, "### Energy Grid"
         for i in range(len_energy-1):
-            print >> self.__file, " %10.3e" % (so.axis[0].val[i])  
+            print >> self.__file, " %10.3e" % (so.axis[0].val[i]),  
 	    if ((i+1) % 8) == 0:
 		print >> self.__file
 	    
