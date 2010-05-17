fe89595ed3115ed54459c47038ac9aa900ca3f75
Michael Reuter <reuterma@ornl.gov>
2010-05-17 17:42:14 +0000

    Merged revisions 971-973,976,978-981,986,996,999-1001 via svnmerge from https://flathead.ornl.gov/repos/DOM/branches/1.7

--- geom/calc_dgs_params.py
+++ geom/calc_dgs_params.py
@@ -49,7 +49,7 @@ timer.getTime(msg="After reading data ")
 SOM_ids = data_dst.get_SOM_ids()
 
 # Get the bank numbers sorted in proper order
-bank_list = [SOM_id[0].split('/')[-1] for SOM_id in SOM_ids]
+bank_list = [SOM_id[0].split('/')[-1] for SOM_id in SOM_ids if SOM_id[1] == 1]
 bank_nums = [int(id.replace('bank', '')) for id in bank_list
              if not id.startswith("monitor")]
 bank_nums.sort()
